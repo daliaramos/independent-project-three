9945dd67b3b033c123c1089a42c5a7bcdc2f9ed5
dalia ramos <dalia.ramoshernandez@gmail.com>
2018-01-19 15:44:36 -0800

    keep adding js.

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,14 @@ $(document).ready(function(){
   });
   var beepBoop = function(number) {
     if (number === 0){
-      return "boop"
+      number += "boop"
+      return number
     }
     else if (number === 1){
       return "beep"
     }
 
-    else if (number/3) {
+    else if ((number % 3)===0) {
     return "Im sorry, Im afraid I can not do that."
     }
       }
