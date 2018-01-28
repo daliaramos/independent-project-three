e54e1d9576d8e466b378b44a4996cd0ae49b7bd0
dalia ramos <dalia.ramoshernandez@gmail.com>
2018-01-28 15:13:22 -0800

    add to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
+
+var array = [];
+
+function beepBoop(number) {
+  for(var i=0; i <= number; i++) {
+    if (i % 3 == 0)
+      array.push("sorry dave");
+    else if (i.toString().includes("0"))
+      array.push("beep");
+    else if (i.toString().includes('1'))
+      array.push("boop");
+    else
+      array.push(i)
+  }
+
+return array;
+}
+//busineslogic
+
 $(document).ready(function(){
   $("form#beep-Boop").submit(function(event){
     event.preventDefault();
-    var number = parseInt($("input#number").val());
+    var number = $("input#number").val();
     var result = beepBoop(number);
     $("#result").text(result);
       event.preventDefault();
 });
-   var beepBoop = function(number) {
-    if (number == 0){
-      return "boop";
-    }
-    else if (number == 1){
-      return number;
-    }
-    else if ((number % 3)===0) {
-    return "Im sorry, Im afraid I can not do that."
-    }
-
-    }
-  });
+});
